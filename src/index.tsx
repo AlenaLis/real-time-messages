5a804710f248f13f21cb61701a64c54c6fe1845e
AlenaLis <alenalisovenko2000@gmail.com>
2022-04-17 01:09:51 +0300

    Added lint, more logic with requests, connected authentication, registration, adding new rooms and function for opening rooms

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,14 @@ import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
@@ -6,25 +6,53 @@ import { Link } from 'react-router-dom'
 import './styles.scss'
 
 const Header = () => {
-    return (
-        <header className="header">
-         <div className='header-left-side'>
-             <Link to='/' className='link'>
-                 Real Time Messenger
-             </Link>
-         </div>
-         <div className='header-right-side'>
-            <div>
-                <Link to='/auth'>
-                    <Button variant="outlined" className='sign-button'>Sign In</Button>
-                </Link>
-                <Link to='/registration'>
-                    <Button variant="outlined" className='reg-button'>Registration</Button>
-                </Link>
-            </div>
-         </div>
-        </header>
-    );
+  const [auth, setAuth] = useState(false)
+
+  const token = localStorage.getItem('CC_Token')
+
+  useEffect(() => {
+    if (token && token?.length > 0) {
+      setAuth(true)
+    } else {
+      setAuth(false)
+    }
+  }, [token])
+
+  const LogOut = () => {
+    localStorage.removeItem('token')
+    window.location.reload()
+  }
+
+  return (
+    <header className="header">
+      <div className="header-left-side">
+        <Link to="/" className="link">
+          Real Time Messenger
+        </Link>
+      </div>
+
+      <div className="header-right-side">
+        {!auth ? (
+          <div>
+            <Link to="/auth">
+              <Button variant="outlined" className="sign-button">
+                Sign In
+              </Button>
+            </Link>
+            <Link to="/registration">
+              <Button variant="outlined" className="reg-button">
+                Registration
+              </Button>
+            </Link>
+          </div>
+        ) : (
+          <Button variant="outlined" className="reg-button" onClick={LogOut}>
+            Log Out
+          </Button>
+        )}
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
